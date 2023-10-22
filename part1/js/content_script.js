e7020dcb6e665fdde30974749fdea807420ed6e5
GETYAMAME <pon.mikan10@gmail.com>
2023-10-22 13:37:59 +0900

    feat:クリックすることにより上部・下部のアラートの表示・非表示を切り替える処理を追加

--- part1/js/content_script.js
+++ part1/js/content_script.js
@@ -1,35 +1,33 @@
-// 一番下へ移動
-function scrollBottom() {
-  let elm = document.documentElement;
-
-  // scrollHeight ページの高さ
-  let bottom = elm.scrollHeight;
-
-  // 垂直方向へ移動
-  window.scroll(0, bottom);
+// 上部の警告クリック時の処理
+function clickTop() {
+  // 下部を表示、上部を非表示にする
+  document.querySelector("#page_bottom_alert").style.display = "block";
+  document.querySelector("#page_top_alert").style.display = "none";
 }
 
-// 一番上へ移動
-function scrollTop() {
-  // 垂直方向へ移動
-  window.scroll(0, 0);
+// 下部の警告クリック時の処理
+function clickBottom() {
+  // 上部を表示、下部を非表示にする
+  document.querySelector("#page_bottom_alert").style.display = "none";
+  document.querySelector("#page_top_alert").style.display = "block";
 }
 
 // 開いているページのbodyを取得
 const body = document.querySelector("body");
 
-// ボタン(top)を作成
+// 上部の警告を作成
 let top_button = document.createElement("div");
 top_button.textContent = "本番環境";
-top_button.addEventListener("click", scrollTop); // クリックされたときの処理を追加
-top_button.className = "page_top_alert"; // CSSで右下に固定するためにクラス名を設定する
+top_button.addEventListener("click", clickTop); // クリックされたときの処理を追加
+top_button.id = "page_top_alert";
 
-// ボタン(bottom)を作成
+// 下部の警告を作成
 let bottom_button = document.createElement("div");
 bottom_button.textContent = "本番環境";
-bottom_button.addEventListener("click", scrollBottom); // クリックされたときの処理を追加
-bottom_button.className = "page_bottom_alert"; // CSSで右下に固定するためにクラス名を設定する
+bottom_button.addEventListener("click", clickBottom); // クリックされたときの処理を追加
+bottom_button.id = "page_bottom_alert";
+bottom_button.style.display = "none"; // 初期状態では非表示
 
-// ボタンをbodyに追加
+// 警告をbodyに追加
 body.append(top_button);
 body.append(bottom_button);
