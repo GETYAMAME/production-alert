c66ff503d46ef661b76fc2a6f1fec7c56d18212b
GETYAMAME <pon.mikan10@gmail.com>
2023-10-22 16:50:05 +0900

    feat:値（ドメイン）の保存・取得を実装

--- part1/js/content_script.js
+++ part1/js/content_script.js
@@ -16,22 +16,51 @@ function clickBottom() {
   document.querySelector("#page_top_alert").style.display = "block";
 }
 
-// 開いているページのbodyを取得
-const body = document.querySelector("body");
+// 本番ドメイン情報を取得する
+function getStorageData(defaults) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(defaults, (items) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(items);
+      }
+    });
+  });
+}
 
-// 上部の警告を作成
-let top_button = document.createElement("div");
-top_button.textContent = "本番環境";
-top_button.addEventListener("click", clickTop); // クリックされたときの処理を追加
-top_button.id = "page_top_alert";
+// 初期表示時の処理
+window.onload = async () => {
+  // 本番ドメインを取得
+  let defaults = { domain: { value: "" } };
+  try {
+    const storageData = await getStorageData(defaults);
+    const prod_domains = storageData.domain.value;
+    const prod_domains_ary = prod_domains.split(",");
+    const current_domain = location.hostname; // 現在表示している画面のドメイン名を取得
+    // 本番ドメインに含まれている場合
+    if (prod_domains_ary.includes(current_domain)) {
+      // 開いているページのbodyを取得
+      const body = document.querySelector("body");
 
-// 下部の警告を作成
-let bottom_button = document.createElement("div");
-bottom_button.textContent = "本番環境";
-bottom_button.addEventListener("click", clickBottom); // クリックされたときの処理を追加
-bottom_button.id = "page_bottom_alert";
-bottom_button.style.display = "none"; // 初期状態では非表示
+      // 上部の警告を作成
+      let top_button = document.createElement("div");
+      top_button.textContent = "本番環境";
+      top_button.addEventListener("click", clickTop); // クリックされたときの処理を追加
+      top_button.id = "page_top_alert";
 
-// 警告をbodyに追加
-body.append(top_button);
-body.append(bottom_button);
+      // 下部の警告を作成
+      let bottom_button = document.createElement("div");
+      bottom_button.textContent = "本番環境";
+      bottom_button.addEventListener("click", clickBottom); // クリックされたときの処理を追加
+      bottom_button.id = "page_bottom_alert";
+      bottom_button.style.display = "none"; // 初期状態では非表示
+
+      // 警告をbodyに追加
+      body.append(top_button);
+      body.append(bottom_button);
+    }
+  } catch (error) {
+    alert("Error fetching data: " + error);
+  }
+};
--- part1/js/popup_script.js
+++ part1/js/popup_script.js
@@ -3,24 +3,46 @@
 // =============================
 
 // 初期表示時の処理
-window.onload = function () {
-  // localStorageからドメインを取得
-  let domains = localStorage.getItem("domainsKey");
-  // ドメインが存在する場合
-  if (domains) {
-    // ドメインを表示
-    document.getElementById("domains").value = domains;
+window.onload = async () => {
+  // 本番ドメインを取得
+  let defaults = { domain: { value: "" } };
+  try {
+    const storageData = await getStorageData(defaults);
+    const domains = storageData.domain.value;
+    if (domains) {
+      // ドメインを表示
+      document.getElementById("domains").value = domains;
+    }
+  } catch (e) {
+    alert("Error fetching data: " + error);
   }
 };
 
 // 保存ボタン押下時処理
-document.getElementById("hold").onclick = function () {
+document.getElementById("hold").onclick = async function () {
   // ドメインを取得
   let domains = document.getElementById("domains").value;
   // エラーチェック処理
 
   // localStorageに保存
-  localStorage.setItem("domainsKey", domains);
-
-  // 保存完了メッセージを表示
+  let entity = {};
+  entity.domain = {
+    value: domains,
+  };
+  await chrome.storage.local.set(entity, function () {
+    alert("保存しました");
+  });
 };
+
+// 本番ドメイン情報を取得する
+function getStorageData(defaults) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(defaults, (items) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(items);
+      }
+    });
+  });
+}
